f9475df19af870861f884de3fe74dc61b9ad920f
Cepluki-Daniel <47762092+Cepluki-Daniel@users.noreply.github.com>
2020-09-24 20:11:01 -0300

    Add files via upload

---
+++ SQL_Teste1/SQL_Teste1/CadastroCliente.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace SQL_Teste1
+{
+    partial class CadastroUsuário
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeCliente = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblCadastroUsuario = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblNascimento = new System.Windows.Forms.Label();
+            this.lblCNH = new System.Windows.Forms.Label();
+            this.txtCNH = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.boxNascimento = new System.Windows.Forms.MaskedTextBox();
+            this.btnTelaUsuario = new System.Windows.Forms.Button();
+            this.btnTelaVeiculo = new System.Windows.Forms.Button();
+            this.btnTelaFuncionario = new System.Windows.Forms.Button();
+            this.btnTelaLocar = new System.Windows.Forms.Button();
+            this.btnTelaFrota = new System.Windows.Forms.Button();
+            this.btnTelaFinanceiro = new System.Windows.Forms.Button();
+            this.btnTelaManutencao = new System.Windows.Forms.Button();
+            this.boxCPF = new System.Windows.Forms.MaskedTextBox();
+            this.boxTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lblNomeCliente
+            // 
+            this.lblNomeCliente.AutoSize = true;
+            this.lblNomeCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCliente.Location = new System.Drawing.Point(69, 120);
+            this.lblNomeCliente.Name = "lblNomeCliente";
+            this.lblNomeCliente.Size = new System.Drawing.Size(148, 25);
+            this.lblNomeCliente.TabIndex = 0;
+            this.lblNomeCliente.Text = "Nome completo";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(260, 120);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(395, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lblCadastroUsuario
+            // 
+            this.lblCadastroUsuario.AutoSize = true;
+            this.lblCadastroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastroUsuario.Location = new System.Drawing.Point(296, 24);
+            this.lblCadastroUsuario.Name = "lblCadastroUsuario";
+            this.lblCadastroUsuario.Size = new System.Drawing.Size(318, 39);
+            this.lblCadastroUsuario.TabIndex = 2;
+            this.lblCadastroUsuario.Text = "Cadastro de Cliente";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(107, 180);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(121, 25);
+            this.lblCPF.TabIndex = 3;
+            this.lblCPF.Text = "CPF / CNPJ";
+            // 
+            // lblNascimento
+            // 
+            this.lblNascimento.AutoSize = true;
+            this.lblNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNascimento.Location = new System.Drawing.Point(34, 240);
+            this.lblNascimento.Name = "lblNascimento";
+            this.lblNascimento.Size = new System.Drawing.Size(185, 25);
+            this.lblNascimento.TabIndex = 5;
+            this.lblNascimento.Text = "Data de nascimento";
+            // 
+            // lblCNH
+            // 
+            this.lblCNH.AutoSize = true;
+            this.lblCNH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCNH.Location = new System.Drawing.Point(148, 300);
+            this.lblCNH.Name = "lblCNH";
+            this.lblCNH.Size = new System.Drawing.Size(82, 25);
+            this.lblCNH.TabIndex = 9;
+            this.lblCNH.Text = "N° CNH";
+            // 
+            // txtCNH
+            // 
+            this.txtCNH.Location = new System.Drawing.Point(260, 300);
+            this.txtCNH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCNH.Name = "txtCNH";
+            this.txtCNH.Size = new System.Drawing.Size(232, 22);
+            this.txtCNH.TabIndex = 10;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(260, 360);
+            this.txtEndereco.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(395, 22);
+            this.txtEndereco.TabIndex = 11;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(129, 360);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(96, 25);
+            this.lblEndereco.TabIndex = 12;
+            this.lblEndereco.Text = "Endereço";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(136, 420);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(89, 25);
+            this.lblTelefone.TabIndex = 13;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(166, 480);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(60, 25);
+            this.lblEmail.TabIndex = 14;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(260, 480);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(232, 22);
+            this.txtEmail.TabIndex = 16;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(391, 580);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(156, 40);
+            this.btnCadastrar.TabIndex = 17;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // boxNascimento
+            // 
+            this.boxNascimento.Location = new System.Drawing.Point(260, 240);
+            this.boxNascimento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.boxNascimento.Mask = "00/00/0000";
+            this.boxNascimento.Name = "boxNascimento";
+            this.boxNascimento.Size = new System.Drawing.Size(89, 22);
+            this.boxNascimento.TabIndex = 18;
+            this.boxNascimento.ValidatingType = typeof(System.DateTime);
+            // 
+            // btnTelaUsuario
+            // 
+            this.btnTelaUsuario.Location = new System.Drawing.Point(30, 689);
+            this.btnTelaUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaUsuario.Name = "btnTelaUsuario";
+            this.btnTelaUsuario.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaUsuario.TabIndex = 19;
+            this.btnTelaUsuario.Text = "Cadastrar Usuário";
+            this.btnTelaUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnTelaVeiculo
+            // 
+            this.btnTelaVeiculo.Location = new System.Drawing.Point(163, 689);
+            this.btnTelaVeiculo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaVeiculo.Name = "btnTelaVeiculo";
+            this.btnTelaVeiculo.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaVeiculo.TabIndex = 20;
+            this.btnTelaVeiculo.Text = "Cadastrar Veículo";
+            this.btnTelaVeiculo.UseVisualStyleBackColor = true;
+            this.btnTelaVeiculo.Click += new System.EventHandler(this.btnTelaVeiculo_Click);
+            // 
+            // btnTelaFuncionario
+            // 
+            this.btnTelaFuncionario.Location = new System.Drawing.Point(297, 689);
+            this.btnTelaFuncionario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaFuncionario.Name = "btnTelaFuncionario";
+            this.btnTelaFuncionario.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaFuncionario.TabIndex = 21;
+            this.btnTelaFuncionario.Text = "Cadastrar Funcionário";
+            this.btnTelaFuncionario.UseVisualStyleBackColor = true;
+            this.btnTelaFuncionario.Click += new System.EventHandler(this.btnTelaFuncionario_Click_1);
+            // 
+            // btnTelaLocar
+            // 
+            this.btnTelaLocar.Location = new System.Drawing.Point(430, 689);
+            this.btnTelaLocar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaLocar.Name = "btnTelaLocar";
+            this.btnTelaLocar.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaLocar.TabIndex = 22;
+            this.btnTelaLocar.Text = "Locar veículo";
+            this.btnTelaLocar.UseVisualStyleBackColor = true;
+            this.btnTelaLocar.Click += new System.EventHandler(this.btnTelaLocar_Click_1);
+            // 
+            // btnTelaFrota
+            // 
+            this.btnTelaFrota.Location = new System.Drawing.Point(563, 689);
+            this.btnTelaFrota.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaFrota.Name = "btnTelaFrota";
+            this.btnTelaFrota.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaFrota.TabIndex = 23;
+            this.btnTelaFrota.Text = "Controle da Frota";
+            this.btnTelaFrota.UseVisualStyleBackColor = true;
+            this.btnTelaFrota.Click += new System.EventHandler(this.btnTelaFrota_Click);
+            // 
+            // btnTelaFinanceiro
+            // 
+            this.btnTelaFinanceiro.Location = new System.Drawing.Point(696, 689);
+            this.btnTelaFinanceiro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaFinanceiro.Name = "btnTelaFinanceiro";
+            this.btnTelaFinanceiro.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaFinanceiro.TabIndex = 24;
+            this.btnTelaFinanceiro.Text = "Financeiro";
+            this.btnTelaFinanceiro.UseVisualStyleBackColor = true;
+            this.btnTelaFinanceiro.Click += new System.EventHandler(this.btnTelaFinanceiro_Click);
+            // 
+            // btnTelaManutencao
+            // 
+            this.btnTelaManutencao.Location = new System.Drawing.Point(829, 689);
+            this.btnTelaManutencao.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTelaManutencao.Name = "btnTelaManutencao";
+            this.btnTelaManutencao.Size = new System.Drawing.Size(133, 64);
+            this.btnTelaManutencao.TabIndex = 25;
+            this.btnTelaManutencao.Text = "Manutenção";
+            this.btnTelaManutencao.UseVisualStyleBackColor = true;
+            this.btnTelaManutencao.Click += new System.EventHandler(this.btnTelaManutencao_Click);
+            // 
+            // boxCPF
+            // 
+            this.boxCPF.Location = new System.Drawing.Point(260, 180);
+            this.boxCPF.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.boxCPF.Mask = "000.000.000-00";
+            this.boxCPF.Name = "boxCPF";
+            this.boxCPF.Size = new System.Drawing.Size(134, 22);
+            this.boxCPF.TabIndex = 26;
+            // 
+            // boxTelefone
+            // 
+            this.boxTelefone.Location = new System.Drawing.Point(260, 420);
+            this.boxTelefone.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.boxTelefone.Mask = "(99) 00000-0000";
+            this.boxTelefone.Name = "boxTelefone";
+            this.boxTelefone.Size = new System.Drawing.Size(117, 22);
+            this.boxTelefone.TabIndex = 27;
+            // 
+            // CadastroUsuário
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.Controls.Add(this.boxTelefone);
+            this.Controls.Add(this.boxCPF);
+            this.Controls.Add(this.btnTelaManutencao);
+            this.Controls.Add(this.btnTelaFinanceiro);
+            this.Controls.Add(this.btnTelaFrota);
+            this.Controls.Add(this.btnTelaLocar);
+            this.Controls.Add(this.btnTelaFuncionario);
+            this.Controls.Add(this.btnTelaVeiculo);
+            this.Controls.Add(this.btnTelaUsuario);
+            this.Controls.Add(this.boxNascimento);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtCNH);
+            this.Controls.Add(this.lblCNH);
+            this.Controls.Add(this.lblNascimento);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.lblCadastroUsuario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNomeCliente);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "CadastroUsuário";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Usuário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeCliente;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblCadastroUsuario;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblNascimento;
+        private System.Windows.Forms.Label lblCNH;
+        private System.Windows.Forms.TextBox txtCNH;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.MaskedTextBox boxNascimento;
+        private System.Windows.Forms.Button btnTelaUsuario;
+        private System.Windows.Forms.Button btnTelaVeiculo;
+        private System.Windows.Forms.Button btnTelaFuncionario;
+        private System.Windows.Forms.Button btnTelaLocar;
+        private System.Windows.Forms.Button btnTelaFrota;
+        private System.Windows.Forms.Button btnTelaFinanceiro;
+        private System.Windows.Forms.Button btnTelaManutencao;
+        private System.Windows.Forms.MaskedTextBox boxCPF;
+        private System.Windows.Forms.MaskedTextBox boxTelefone;
+    }
+}
+
